996a309cbfac672474d9b534be9c708f3d06419a
Desnoc <48734249+Desnoc@users.noreply.github.com>
2019-04-20 19:08:18 +0200

    Update reprifikabot.js

--- .gitignore/reprifikabot.js
+++ .gitignore/reprifikabot.js
@@ -75,5 +75,94 @@ bot.on('message', message => {
       }
     }
   });
+
+bot.on('message', message => {
+  // Ignore les messages qui ne proviennent pas d'une guilde
+  if (!message.guild) return;
+
+  // Si le contenu du message commence par "!Kick"
+  if (message.content.startsWith('!kick')) {
+  //En supposant que nous mentionnons quelqu'un dans le message, cela va retourner l'utilisateur
+  // En savoir plus sur les mentions plus à https://discord.js.org/#/docs/main/stable/class/MessageMentions
+    const user = message.mentions.users.first();
+    //    Si nous avons un utilisateur mentionné
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member.kick('Optional reason that will display in the audit logs').then(() => {
+          // We let the message author know we were able to kick the person
+          message.reply(`${user.tag} à été kick avec succès !`);
+        }).catch(err => {
+          // An error happened
+          // This is generally due to the bot not being able to kick the member,
+          // either due to missing permissions or role hierarchy
+          message.reply('Je ne peux pas kick ce membre !');
+          // Log the error
+          console.error(err);
+        });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply(`Cet utilisateur n'est pas sur le serveur !`);
+      }
+    // Otherwise, if no user was mentioned
+    } else {
+      message.reply(`Vous n'avez pas mentionné l'utilisateur que je devais kick !`);
+    }
+  }
+});
+
+
+  bot.on('message', message => {
+    // Ignore messages that aren't from a guild
+    if (!message.guild) return;
+  
+    // if the message content starts with "!ban"
+    if (message.content.startsWith('!ban')) {
+      // Assuming we mention someone in the message, this will return the user
+      // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
+      const user = message.mentions.users.first();
+      // If we have a user mentioned
+      if (user) {
+        // Now we get the member from the user
+        const member = message.guild.member(user);
+        // If the member is in the guild
+        if (member) {
+          /**
+           * Ban the member
+           * Make sure you run this on a member, not a user!
+           * There are big differences between a user and a member
+           * Read more about what ban options there are over at
+           * https://discord.js.org/#/docs/main/stable/class/GuildMember?scrollTo=ban
+           */
+          member.ban({
+            reason: 'They were bad!',
+          }).then(() => {
+            // We let the message author know we were able to ban the person
+            message.reply(`${user.tag} à été bannis avec succès !`);
+          }).catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to ban the member,
+            // either due to missing permissions or role hierarchy
+            message.reply(`Je n'ai pas les permissions pour ban ce membre !`);
+            // Log the error
+            console.error(err);
+          });
+        } else {
+          // The mentioned user isn't in this guild
+          message.reply(`Cet utilisateur n'est pas sur le serveur !`);
+        }
+      } else {
+      // Otherwise, if no user was mentioned
+        message.reply(`Vous n'avez pas mentionné l'utilisateur que je devais bannir !`);
+      }
+    }
+  });
  
 bot.login(process.env.TOKEN)
