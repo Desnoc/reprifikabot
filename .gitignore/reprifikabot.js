4c4bc222963439b5993b6e7d32597ffe1fd25eac
Desnoc <48734249+Desnoc@users.noreply.github.com>
2019-04-20 18:58:23 +0200

    Update reprifikabot.js

--- .gitignore/reprifikabot.js
+++ .gitignore/reprifikabot.js
@@ -76,29 +76,36 @@ bot.on('message', message => {
     }
   });
 
-
-
-client.on('message', message => {
-    if (!message.guild) return;
-
-    if (message.content.startsWith('*kick')) {
-      const user = message.mentions.users.first();
-      if (user) {
-        const member = message.guild.member(user);
-        if (member) {
-          member.kick('Optional reason that will display in the audit logs').then(() => {
-            message.reply(`Renvoyé avec succès ${user.tag}`);
-          }).catch(err => {
-            message.reply('Je ne peux pas kick ce membre');
-            // Log the error
-            console.error(err);
-          });
-        } else {
-          message.reply("Cette personne n'est pas sur le serveur");
-        }
-      } else {
-        message.reply("Vous n'avez mentionné personne");
-      }
+/*Kick*/
+client.on('message',message =>{
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+ 
+    if (args[0].toLocaleLowerCase() === prefix + 'kick'){
+       if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande ;(")
+       let member = message.mentions.members.first()
+       if (!member) return message.channel.send("Veuillez mentionner un utilisateur :x:")
+       if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("Vous ne pouvez pas kick cet utilisateur :x:")
+       if (!member.kickable) return message.channel.send("Je ne peux pas exclure cet utilisateur :sunglass:")
+       member.kick()
+       message.channel.send("**"+member.user.username + '** a été exclu :white_check_mark:')
     }
+});
+ 
+/*Ban*/
+client.on('message',message =>{
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+ 
+    if (args[0].toLocaleLowerCase() === prefix + 'ban'){
+       if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande ;(")
+       let member = message.mentions.members.first()
+       if (!member) return message.channel.send("Veuillez mentionner un utilisateur :x:")
+       if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("Vous ne pouvez pas bannir cet utilisateur :x:")
+       if (!member.bannable) return message.channel.send("Je ne peux pas bannir cet utilisateur :sunglass:")
+       message.guild.ban(member, {days: 7})
+       message.channel.send("**"+member.user.username + '** a été banni :white_check_mark:')
+    }
+});
  
 bot.login(process.env.TOKEN)
