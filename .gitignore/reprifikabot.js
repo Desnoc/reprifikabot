7d089e6d08ed6dcbe73863cca53d174a811f3319
Desnoc <48734249+Desnoc@users.noreply.github.com>
2019-04-20 18:14:53 +0200

    Update reprifikabot.js

--- .gitignore/reprifikabot.js
+++ .gitignore/reprifikabot.js
@@ -1,11 +1,9 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
-const Google = require('./commands/google.js')
-const Youtube = require('./commands/youtube.js')
- 
- 
- 
- 
+
+var prefix = ('*')
+
+
 bot.on('ready', function () {
     bot.user.setGame('création de bot Discord').catch(console.error) 
 })
@@ -16,124 +14,21 @@ bot.on('guildMemberAdd', function (member) {
  
     }).catch(console.error)
 })
- 
- 
- 
-bot.on('message', function (message) {
-    if (message.content === '!help') {
-        message.channel.send('liendemapageweb')
-    }
-})
- 
- 
- 
-bot.on('message', function (message) {
-    if (Youtube.match(message)) {
-        return Youtube.action(message)
-    }
-    if (message.content === 'r!help') {
-        message.channel.send('Le prefix est : r! : kisaru; reprifika; web')
-    }
-})
- 
- 
- 
-bot.on('message', function (message) {
-    if (message.content === 'r!kisaru') {
-        message.channel.send('https://www.youtube.com/channel/UCAdr3amBpfsqP7ojSJkbZfA')
-    }
-})
- 
-bot.on('message', function (message) {
-    if (message.content === 'r!reprifika') {
-        message.channel.send('')
-    }
-})
- 
- 
-bot.on('message', function (message) {
-    if (message.content === 'r!web') {
-        message.channel.send('')
-    }
-})
- 
-bot.on('message', function (message) {
-    if (message.content === '') {
-        message.channel.send('')
-    }
-})
- 
- 
- 
-bot.on('message', function (message) {
-    if (message.content === '') {
-        message.channel.send('')
-    }
-})
- 
- 
-bot.on('message', function (message) {
-    if (message.content === '') {
-        message.channel.send('')
-    }
-})
- 
- 
-bot.on('message', function (message) {
-    if (message.content === '') {
-        message.channel.send('')
-    }
-})
- 
- 
- 
-bot.on('message', function (message) {
-    if (message.content === '') {
-        message.channel.send('')
-    }
-})
- 
- 
-bot.on('message', function (message) {
-    if (Google.match(message)) {
-        return Google.action(message)
-    }
-})
 
-Bot.on("message", async message => {
+bot.on("message", message => {
+	if(message.content === prefix + "help"){
+		message.channel.sendMessage("Liste des commandes : *tt/ *fabriquant");
+	}
 
-    if(command === "r!mute"){
-  
-      if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Vous n'avez pas les droits pour muter un utilisateur !");
-  
-      let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-      if(!toMute) return message.channel.send("Merci d'entrer un utilisateur !");
-      let role = message.guild.roles.find(r => r.name === "Utilisateurs mutés");
-      if(!role){
-        try {
-          role = await message.guild.createRole({
-            name: "Utilisateurs mutés",
-            color:"#000000",
-            permissions:[]
-          });
-  
-          message.guild.channels.forEach(async (channel, id) => {
-            await channel.overwritePermissions(role, {
-              SEND_MESSAGES: false,
-              ADD_REACTIONS: false
-            });
-          });
-        } catch (e) {
-          console.log(e.stack)
-        }
-      }
-  
-      if(toMute.roles.has(role.id)) return message.channel.send('Cet utilisateur est déjà muté !');
-  
-      await(toMute.addRole(role));
-      message.channel.send("Je l'ai muté !");
-    }
-    
-})
+	if(message.content === prefix + "tt"){
+		message.channel.sendMessage("Bot créer le _20/04/2019_ à _18h00");
+		console.log("Commande éffectuée");
+	}
+
+	if(message.content === prefix + "fabriquant"){
+		message.channel.sendMessage("Le bot à été créé par Desnoc");
+		console.log("Commande éffectuée");
+	}
+});
  
-bot.login('NTU3MTA2OTUxNzQ2NzQ4NDI2.D3LofQ.nNep2WbFU_UPSjG4uRznLj5NqG0')
+bot.login(process.env.TOKEN)
