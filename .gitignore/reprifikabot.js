2c64dc667c2a4404cb881ba2a7afd933e3a7a1f4
Desnoc <48734249+Desnoc@users.noreply.github.com>
2019-04-20 18:43:59 +0200

    Update reprifikabot.js

--- .gitignore/reprifikabot.js
+++ .gitignore/reprifikabot.js
@@ -31,87 +31,74 @@ bot.on("message", message => {
 	}
 });
 bot.on('message', message => {
-    // Ignore messages that aren't from a guild
     if (!message.guild) return;
   
-    // If the message content starts with "!kick"
     if (message.content.startsWith('*mute')) {
-      // Assuming we mention someone in the message, this will return the user
-      // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
       const user = message.mentions.users.first();
-      // If we have a user mentioned
       if (user) {
-        // Now we get the member from the user
         const member = message.guild.member(user);
-        // If the member is in the guild
         if (member) {
-          /**
-           * Kick the member
-           * Make sure you run this on a member, not a user!
-           * There are big differences between a user and a member
-           */
-          member.mute('Optional reason that will display in the audit logs').then(() => {
-            // We let the message author know we were able to kick the person
+          member.mute("Raison facultative à afficher dans les journaux d'audit").then(() => {
             message.reply(`Successfully muted ${user.tag}`);
           }).catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
-            message.reply('I was unable to mute the member');
-            // Log the error
+            message.reply('Je suis incapable de mettre le membre en sourdine');
             console.error(err);
           });
         } else {
-          // The mentioned user isn't in this guild
-          message.reply('That user isn\'t in this guild!');
+          message.reply("Cet utilisateur n'est pas dans cette guilde!");
         }
-      // Otherwise, if no user was mentioned
       } else {
-        message.reply('You didn\'t mention the user to mute!');
+        message.reply("Vous n'avez pas mentionné l'utilisateur à mettre en sourdine!");
       }
     }
   });
 
   bot.on('message', message => {
-    // Ignore messages that aren't from a guild
     if (!message.guild) return;
   
-    // If the message content starts with "!kick"
     if (message.content.startsWith('*unmute')) {
-      // Assuming we mention someone in the message, this will return the user
-      // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
       const user = message.mentions.users.first();
-      // If we have a user mentioned
       if (user) {
-        // Now we get the member from the user
         const member = message.guild.member(user);
-        // If the member is in the guild
         if (member) {
-          /**
-           * Kick the member
-           * Make sure you run this on a member, not a user!
-           * There are big differences between a user and a member
-           */
-          member.unmute('Optional reason that will display in the audit logs').then(() => {
-            // We let the message author know we were able to kick the person
+          member.unmute("Raison facultative à afficher dans les journaux d'audit").then(() => {
             message.reply(`Successfully unmuted ${user.tag}`);
           }).catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
-            message.reply('I was unable to unmute the member');
+            message.reply('Je suis incapable de redonner la parole à ce membre');
+            console.error(err);
+          });
+        } else {
+          message.reply("Ce membre n'est plus sur le serveur");
+        }
+      } else {
+        message.reply("Vous n'avez pas mentionné l'utilisateur pour réactiver la parole!");
+      }
+    }
+  });
+
+
+
+client.on('message', message => {
+    if (!message.guild) return;
+
+    if (message.content.startsWith('*kick')) {
+      const user = message.mentions.users.first();
+      if (user) {
+        const member = message.guild.member(user);
+        if (member) {
+          member.kick('Optional reason that will display in the audit logs').then(() => {
+            message.reply(`Renvoyé avec succès ${user.tag}`);
+          }).catch(err => {
+            message.reply('Je ne peux pas kick ce membre');
             // Log the error
             console.error(err);
           });
         } else {
-          // The mentioned user isn't in this guild
-          message.reply('That user isn\'t in this guild!');
+          message.reply("Cette personne n'est pas sur le serveur");
         }
-      // Otherwise, if no user was mentioned
       } else {
-        message.reply('You didn\'t mention the user to unmute!');
+        message.reply("Vous n'avez mentionné personne");
       }
     }
-  });
  
 bot.login(process.env.TOKEN)
