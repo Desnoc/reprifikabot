cdf9c2016baf0fbd12903a8e7d11bdb556245d67
Desnoc <48734249+Desnoc@users.noreply.github.com>
2019-04-26 16:20:35 +0200

    Update reprifikabot.js

--- .gitignore/reprifikabot.js
+++ .gitignore/reprifikabot.js
@@ -167,8 +167,8 @@ bot.on('message', message => {
 
 bot.on("message", message => {
     if(message.content[0] === prefix){
-        if(message.content == prefix + args){
-            message.channel.send(args);
+        if(message.content == prefix + message){
+            message.channel.send(args + message);
         }
     }
 });
